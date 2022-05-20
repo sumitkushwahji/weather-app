ec46f27ce8e2c4f89f0289c5dbb28faf5276cdbc
Sumit Kushwah <sumitkushwahji@gmail.com>
2022-05-20 16:00:30 +0530

    final commit

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const hbs =require("hbs");
 const app =express();
 
-const port =process.env.PORT || 8080;
+const port =process.env.PORT || 80;
 
 
 const staticPath= path.join(__dirname,"../public");
