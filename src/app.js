0bde04a3c7c12dbbfcaf6a2d4ad97e60c17d7bbe
Sumit kushwa <sumitkushwahji@gmail.com>
2023-09-19 10:34:37 +0530

    code of bar chart graph added

--- src/app.js
+++ src/app.js
@@ -1,4 +1,7 @@
 const { static } = require("express");
+const WebSocket = require('ws');
+
+const { SerialPort } = require('serialport')
 const path = require("path");
 const express = require("express");
 const hbs =require("hbs");
@@ -20,6 +23,48 @@ hbs.registerPartials(partials_Path);
 
 app.use(express.static(staticPath));
 
+
+
+// code for getting data of gnss----------------------------------------------------------------------------
+
+// // create a new serial port instance
+ const serialport = new SerialPort({ path: 'COM3', baudRate: 9600 })
+
+// // // create a new WebSocket server instance
+  const wss = new WebSocket.Server({ port: 8080 });
+
+  let dataArray = ''
+
+// // // Serial port opened and fetched data
+   serialport.on('data', data => {
+   dataArray += data.toString()
+
+
+// //    // broadcast message to all connected clients
+    wss.clients.forEach(client => {
+        if (client.readyState === WebSocket.OPEN) {
+           client.send(JSON.stringify(message));
+           
+        }
+        });
+      
+})
+
+//  // Log the concatenated data every second
+  setInterval(() => {
+    console.log('------------------------------------------------')
+    const lines = dataArray.split('\n') // Split the string into an array of lines
+    lines.forEach(line => {
+      
+      const fields = line.split(',')//  Split the line into an array of fields
+      const fifthData = fields[4]//  Access the 5th field (index 4)
+      const eighthData = fields[7] // Access the 8th field (index 7)
+      console.log('Received data - PRN:', fifthData, 'SNR:', eighthData, line)
+    })
+  }, 1000) // Add a closing parenthesis here
+
+//-----------------------------------------------------------------------------------------------------
+
 //routing
 app.get("",(req,res)=>{
    res.render("index");
@@ -36,6 +81,10 @@ app.get("/about",(req,res)=>{
  app.get("/covid",(req,res)=>{
    res.render("covid");
 })
+ app.get("/client",(req,res)=>{
+   res.render("client");
+})
+
 
  app.get("*",(req,res)=>{
     res.render("404error",{
